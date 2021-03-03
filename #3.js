e1af6bcd4755640c3300c5903a5eac1d16991aef
josesrodriguez610 <josesrodriguez610@gmail.com>
2021-03-03 15:18:57 -0600

    (add) exercise 3

---
+++ #3.js
@@ -0,0 +1,8 @@
+// #3 If you could change 1 thing about your favorite framework/language/platform (pick one), what would it be?
+
+/* I would pick ReactJs for this question because it is the one that I use the most and I would say it is my favorite.
+  If I could change something it would be to be more live data friendly, which is something I learned from using the
+  Phoenix framework. In Phoenix there are already web sockets and channels ready to be used for building real time
+  applications and in ReactJs you would have to add libraries like socket.io or pusher to serve your realtime features
+  in your applications.
+*/
